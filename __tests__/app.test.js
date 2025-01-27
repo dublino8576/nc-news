aa7cc49821ea7049a72b654fdf7cc45093307675
lucadm_u <lucadimaio@gmail.com>
2025-01-27 19:03:42 +0000

    GET /api/article/:article_id successful request complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,7 @@ const seed = require("../db/seeds/seed");
 /* Set up your test imports here */
 const endpointsJson = require("../endpoints.json");
 const index = require("../db/data/test-data/index");
+const { getArticleById } = require("../controller/controller");
 /* Set up your beforeEach & afterAll functions here */
 beforeEach(() => {
   index;
@@ -51,6 +52,30 @@ describe("GET /api/topics", () => {
       .then((response) => {
         const body = response.body;
         expect(body.error).toBe("Endpoint not found");
+      });
+  });
+});
+
+describe.only("GET /api/articles/:article_id", () => {
+  test("200: Responds with an article object with the corresponding article id", () => {
+    return request(app)
+      .get("/api/articles/7")
+      .expect(200)
+      .then((response) => {
+        const article = response.body;
+        console.log(article);
+        expect(article.title).toBe("Using React Native: One Year Later");
+        expect(article.topic).toBe("coding");
+        expect(article.author).toBe("tickle122");
+        expect(article.body).toBe(
+          "When I interviewed for the iOS developer opening at Discord last spring, the tech lead Stanislav told me: React Native is the future. We will use it to build our iOS app from scratch as soon as it becomes public. As a native iOS developer, I strongly doubted using web technologies to build mobile apps because of my previous experiences with tools like PhoneGap. But after learning and using React Native for a while, I am glad we made that decision."
+        );
+        expect(article.created_at).toBe("2020-10-18T00:26:00.000Z");
+        expect(article.votes).toBe(0);
+        expect(article.article_img_url).toBe(
+          "https://images.pexels.com/photos/6424586/pexels-photo-6424586.jpeg?w=700&h=700"
+        );
+        expect(article).toEqual(article);
       });
   });
 });
