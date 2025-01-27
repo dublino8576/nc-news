fee000388c631795b76066827eca11a5edf5953a
lucadm_u <lucadimaio@gmail.com>
2025-01-27 16:41:28 +0000

    Complete GET /api/topics and error message

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,8 +4,11 @@ const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 /* Set up your test imports here */
 const endpointsJson = require("../endpoints.json");
+const index = require("../db/data/test-data/index");
 /* Set up your beforeEach & afterAll functions here */
-
+beforeEach(() => {
+  index;
+});
 afterAll(() => db.end());
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
@@ -19,6 +22,31 @@ describe("GET /api", () => {
   test('404: Responds with a message of "Endpoint not found"', () => {
     return request(app)
       .get("/api/minimum")
+      .expect(404)
+      .then((response) => {
+        const body = response.body;
+        expect(body.error).toBe("Endpoint not found");
+      });
+  });
+});
+
+describe("GET /api/topics", () => {
+  test("200: Responds with an array of topic objects, each of which should have the slug and description properties", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((response) => {
+        const topics = response.body.topics;
+        expect(topics.length).toBe(3);
+        topics.forEach((topic) => {
+          expect(typeof topic.description).toBe("string");
+          expect(typeof topic.slug).toBe("string");
+        });
+      });
+  });
+  test('404: Responds with an error message of "Endpoint not found" wrong URL being input', () => {
+    return request(app)
+      .get("/api/topic")
       .expect(404)
       .then((response) => {
         const body = response.body;
