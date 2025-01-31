f1c0b4f723a49d6a822ce147ad23f516de27a1b9
lucadm_u <lucadimaio@gmail.com>
2025-01-31 11:03:47 +0000

    Config object assign values for deployment

--- db/connection.js
+++ db/connection.js
@@ -9,5 +9,9 @@ require("dotenv").config({
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
+if (ENV === "production") {
+  config.productionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
 module.exports = new Pool(config);
