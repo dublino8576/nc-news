ed4b3024ef809f5eff5ed1b15ba34abc3ef41e2b
lucadm_u <lucadimaio@gmail.com>
2025-01-31 13:07:10 +0000

    14 hosting troubleshooting 4

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,6 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
-const config = {};
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
@@ -9,6 +8,7 @@ require("dotenv").config({
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
+const config = {};
 if (ENV === "production") {
   config.productionString = process.env.DATABASE_URL;
   config.max = 2;
